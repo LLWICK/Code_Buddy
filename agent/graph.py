3ac5dd56cfc023566ad653116ba3654aee92629c
LLWICK <177215471+LLWICK@users.noreply.github.com>
2026-04-17 07:20:47 +0530

    update

--- agent/graph.py
+++ agent/graph.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 load_dotenv()
 
 llm = ChatGroq(model="openai/gpt-oss-120b");
 
-user_prompt = "create a simple calculator web application"
+user_prompt = "calculator web application"
 
 prompt = f"""
 
@@ -14,10 +14,18 @@ user request : {user_prompt}
 
 """
 
+class File(BaseModel):
+    path: str = Field(description="The path to the file to be created or modified")
+    purpose: str = Field(description="The purpose of the file, e.g. 'main application logic', 'data processing module', etc.")
 
-class Schema(BaseModel):
-    pass
+
+class Plan(BaseModel):
+    name: str = Field(description="The name of app to be built")
+    description: str = Field(description="A oneline description of the app to be built, e.g. 'A web application for managing personal finances'")
+    techstack: str = Field(description="The tech stack to be used for the app, e.g. 'python', 'javascript', 'react', 'flask', etc.")
+    features: list[str] = Field(description="A list of features that the app should have, e.g. 'user authentication', 'data visualization', etc.")
+    files: list[File] = Field(description="A list of files to be created, each with a 'path' and 'purpose'")
     
 
-result = llm.with_structured_output(Schema).invoke(prompt)
+result = llm.with_structured_output(Plan).invoke(prompt)
 print(result)
