738ebc3a891df0af6642eafe62a13939cefbbe3a
LLWICK <177215471+LLWICK@users.noreply.github.com>
2026-04-17 17:34:00 +0530

    update

--- agent/graph.py
+++ agent/graph.py
@@ -1,31 +1,67 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from pydantic import BaseModel, Field
+from langchain_ollama import ChatOllama
+from prompts import *
+from states import *
+from langgraph.graph import StateGraph, END, START
 load_dotenv()
-
-llm = ChatGroq(model="openai/gpt-oss-120b");
-
-user_prompt = "calculator web application"
-
-prompt = f"""
-
-You are a project PLANNER agent . convert the user prompt into a COMPLETE engineering project plan
-user request : {user_prompt}
-
-"""
-
-class File(BaseModel):
-    path: str = Field(description="The path to the file to be created or modified")
-    purpose: str = Field(description="The purpose of the file, e.g. 'main application logic', 'data processing module', etc.")
+import json
+llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0);
 
 
-class Plan(BaseModel):
-    name: str = Field(description="The name of app to be built")
-    description: str = Field(description="A oneline description of the app to be built, e.g. 'A web application for managing personal finances'")
-    techstack: str = Field(description="The tech stack to be used for the app, e.g. 'python', 'javascript', 'react', 'flask', etc.")
-    features: list[str] = Field(description="A list of features that the app should have, e.g. 'user authentication', 'data visualization', etc.")
-    files: list[File] = Field(description="A list of files to be created, each with a 'path' and 'purpose'")
+
+
+user_prompt = "calculator web based app"
+
+prompt = planner_prompt(user_prompt)
+
+def planner_agent(state: agent_state)-> agent_state:
+    print("executing PLANNER agent Node...")
+    resp = llm.with_structured_output(Plan).invoke(state["messages"])
+
+    if resp is None:
+        raise ValueError("Planner did not return a valid response.")
+    
+    
+    return {
+        "messages": [resp]
+    }
+
+def architect_agent(state: agent_state)-> agent_state:
+    
+    print("executing ARCHITECT agent Node...")
+    plan: Plan = state["messages"][-1]
+
+    resp = llm.with_structured_output(TaskPlan).invoke(
+        architect_prompt(state)
+    )
+
+    if resp is None:
+        raise ValueError("Architect did not return a valid response.")
+    
+    else:
+        state['messages'] = state['messages'].append(resp)
+    
+    return state;
     
 
-result = llm.with_structured_output(Plan).invoke(prompt)
-print(result)
+
+    
+
+#result = llm.with_structured_output(Plan).invoke(prompt)
+#print(result)
+
+builder = StateGraph(agent_state)
+
+builder.add_node("planner_agent_node", planner_agent)
+builder.add_node("architect_agent_node", architect_agent)
+
+builder.add_edge(START, "planner_agent_node")
+builder.add_edge("planner_agent_node", "architect_agent_node")
+builder.add_edge("architect_agent_node", END)
+
+graph = builder.compile()
+
+response = graph.invoke({"messages": prompt})
+
+print(response["messages"])
