64267338794e027c950384d65de50fa749a9c68c
LLWICK <177215471+LLWICK@users.noreply.github.com>
2026-04-18 16:33:11 +0530

    created agent state (coder)

--- agent/graph.py
+++ agent/graph.py
@@ -4,11 +4,15 @@ from langchain_ollama import ChatOllama
 from prompts import *
 from states import *
 from langgraph.graph import StateGraph, END, START
+from langchain_core.globals import set_verbose, set_debug
+from langchain.agents import create_agent
 load_dotenv()
 import json
+from tools import *
 llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0);
 
-
+#set_debug(True)
+#set_verbose(True)
 
 
 user_prompt = "calculator web based app"
@@ -33,16 +37,59 @@ def architect_agent(state: agent_state)-> agent_state:
     plan: Plan = state["messages"][-1]
 
     resp = llm.with_structured_output(TaskPlan).invoke(
-        architect_prompt(state)
+        architect_prompt(plan)
     )
 
     if resp is None:
         raise ValueError("Architect did not return a valid response.")
     
-    else:
-        state['messages'] = state['messages'].append(resp)
+    resp.plan = plan
     
-    return state;
+    return {
+        "messages": [resp]
+    }
+
+
+def coder_agent(state: agent_state)-> agent_state:
+    
+    print("executing CODER agent Node...")
+    steps = state["messages"][0].implementation_steps
+
+    current_step_indx = 0
+
+    current_task = steps[current_step_indx]
+
+    existing_content = read_file.run(current_task.filepath)
+
+    user_prmpt = (
+        
+            f"Task:{current_task.task_description}\n"
+            f"File:{current_task.filepath}\n"
+            f"Existing content: {existing_content}"
+            "User write_file(path, content)  to save your changes"
+    
+        
+    )
+
+    system_prompt = coder_system_prompt()
+    Tools = [read_file, write_file, list_files, get_current_directory]
+
+    coderAgent = create_agent(model=llm, tools=Tools)
+
+    coderAgent.invoke({"messages":[{"role":"system", "content":system_prompt},
+                                   {"role":"user", "content":user_prmpt}]})
+
+    
+    
+
+    """ if resp is None:
+        raise ValueError("Architect did not return a valid response.") """
+    
+    
+    
+    return {
+        
+    }
     
 
 
@@ -55,13 +102,17 @@ builder = StateGraph(agent_state)
 
 builder.add_node("planner_agent_node", planner_agent)
 builder.add_node("architect_agent_node", architect_agent)
+builder.add_node("coder_agent_node", coder_agent)
 
 builder.add_edge(START, "planner_agent_node")
 builder.add_edge("planner_agent_node", "architect_agent_node")
-builder.add_edge("architect_agent_node", END)
+builder.add_edge("architect_agent_node", "coder_agent_node")
+builder.add_edge("coder_agent_node", END)
 
 graph = builder.compile()
 
-response = graph.invoke({"messages": prompt})
 
-print(response["messages"])
+
+if __name__ == "__main__":
+    response = graph.invoke({"messages": prompt})
+    print(response["messages"])
--- agent/states.py
+++ agent/states.py
@@ -1,6 +1,6 @@
 
 from pydantic import BaseModel, Field, ConfigDict
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated, Optional
 from langgraph.graph.message import add_messages
 
 class File(BaseModel):
@@ -26,3 +26,8 @@ class ImplementationTask(BaseModel):
 class TaskPlan(BaseModel):
     implementation_steps: list[ImplementationTask] = Field(description="A list of steps to be taken to implement the task")
     model_config = ConfigDict(extra="allow")
+
+class CoderState(BaseModel):
+    task_plan: TaskPlan = Field(description="The plan for the task to be implemented")
+    current_step_idx: int = Field(0, description="The index of the current step in the implementation steps")
+    current_file_content: Optional[str] = Field(None, description="The content of the file currently being edited or created")
